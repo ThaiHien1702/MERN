ee439d5dd0654dc1f366220736a373871e292f4a
ThaiHien170294 <thaihien17294@gmail.com>
2021-05-01 14:35:21 +0700

    hien

--- react-hook/src/App/contexts/TodoContext.js
+++ react-hook/src/App/contexts/TodoContext.js
@@ -1,26 +1,32 @@
-import react, { createContext, useState } from 'react'
+import react, { createContext, useState ,useEffect} from 'react'
 
 export const TodoContext = createContext()
 
 const TodoContextProvider = ({children}) => {
-    const [todos, setTodos] = useState([
-        { id: 1, title: 'viec 1' },
-        { id: 2, title: 'viec 2' },
-        { id: 3, title: 'viec 3' },
-    ])
+    const [todos, setTodos] = useState([])
+    useEffect(() => {
+        const todos = localStorage.getItem('todos')
+        if (todos) setTodos(JSON.parse(todos))
+    
+    }, [])
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
 
     const addTodo = todo => {
         setTodos([...todos, todo])
     }
+
     const deletTodo = (id) => {
         setTodos(todos.filter(todo => todo.id !== id))
-
     }
+
     const TodocontextData = {
         todos,
         addTodo,
         deletTodo
     }
+    
     return(
         <TodoContext.Provider value={TodocontextData}>
             {children}
